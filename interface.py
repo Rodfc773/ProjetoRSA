85602b882d15ee9cc8a2a75e0a939186088b76e6
Rodfc773 <56032656+Rodfc773@users.noreply.github.com>
2020-12-13 14:50:37 -0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -5,19 +5,21 @@ def main():
     print('-------------------------<ESCOLHA UMA DAS OPÇÕES ABAIXO>---------------------------------')
     print('\n')
     print('-----------------------| 1 - CHAVE PUBLICA |----------------------------------------------')
-    print('-----------------------| 2 - ENCRIPTAR |-----------------------------------------------')
-    print('-----------------------| 3 - DESENCRIPTAR |-------------------------------')
-    print('-----------------------| 4 - SAIR |-------------------------------')
+    print('-----------------------| 2 - ENCRIPTAR |--------------------------------------------------')
+    print('-----------------------| 3 - DESENCRIPTAR |-----------------------------------------------')
+    print('-----------------------| 4 - SAIR |-------------------------------------------------------')
     print('\n')
     print('\n')
 
 def error_primos():
 
-    print('------| P OU Q NÃO ERA PRIMO POR FAVOR DIGITE-OS NOVAMENTE |--------------')
+    print('------| P OU Q NÃO ERA PRIMO POR FAVOR DIGITE-OS NOVAMENTE |-------------------------------')
 
 def error_menor_28():
-    print('------| p * q < 28, POR FAVOR DIGITE-OS NOVAMENTE |--------------')
+    print('------| p * q < 28, POR FAVOR DIGITE-OS NOVAMENTE |-----------------------------------------')
 
+def error_iguais():
+    print('------| p e q SÃO IGUAIS,POR FAVOR DIGITE-OS NOVAMENTE | ------------------------------------')
 def erros_e():
         print('-----| (e) É UM VALOR QUE NÃO ATENDE OS REQUISITOS, POR FAVOR DIGITE NOVAMENTE | -----------')
 
